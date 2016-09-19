c75095a919da8d2dab51381bd3b90f8875f4fd0a
Viscus <keegansaint194@hotmail.com>
2016-09-19 11:16:59 -0400

    created a todo list

---
+++ src/com/theironyard/Item.java
@@ -0,0 +1,14 @@
+package com.theironyard;
+
+/**
+ * Created by Viscus on 9/19/16.
+ */
+public class Item {
+    String text;
+    boolean isDone;
+
+    public Item(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+    }
+}
